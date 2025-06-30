4577c68998bfbef442e15fae757226644b7bb00f
Kieren Hussey <khussey45@gmail.com>
2025-06-30 17:34:25 -0400

    added a chatbot

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -123,6 +123,14 @@ const Portfolio = () => {
       </nav>
 
       {/* Main content */}
+      {/* <!-- SalesOptAI Chat Widget --> */}
+<div id="salesoptai-chat" 
+     data-token="web_8332_20250630212045"
+     data-color="#000000"
+     data-title="Chat with us"
+     data-api-url="https://salesoptai2025-backend-ajc8hac0f6aqdvhw.canadacentral-01.azurewebsites.net">
+</div>
+<script src="https://portal.salesoptai.com/chat-embed.js"></script>
       <div className="container mx-auto px-6 pt-20">
         {/* Hero section */}
         <section id="home" className="min-h-screen flex items-center justify-center background-image">
