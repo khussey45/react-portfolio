c6fdc5a01d9762a40693d00b614d1f06e93523b1
Kieren Hussey <khussey45@gmail.com>
2025-07-02 13:53:14 -0400

    uncommented chatbot

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -133,13 +133,13 @@ const Portfolio = () => {
       </div> */}
 
       {/* <!-- SalesOptAI LeadFlow Widget --> */}
-      {/* <div id="salesoptai-chat" 
+      <div id="salesoptai-chat" 
           data-token="leadflow_8332_20250702170017"
           data-color="#000000"
           data-title="Chat with us"
           data-auto-open="true"
           data-api-url="https://salesoptai2025-backend-ajc8hac0f6aqdvhw.canadacentral-01.azurewebsites.net">
-      </div> */}
+      </div>
 
       
 
